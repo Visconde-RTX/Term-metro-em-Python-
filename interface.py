8f02a3bcfd6b7e185e156a652cd5dcac097078c4
Visconde-RTX <moisesvisconde321@gmail.com>
2026-03-19 13:12:47 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,40 @@
+import tkinter as tk 
+from temperatura import Temperatura
+
+def iniciar_interface():
+
+    def converter():
+        c = float(entrada_c.get())
+        f = float(entrada_f.get())
+        k = float(entrada_k.get())
+
+        temp = Temperatura(c, f, 0, k)
+
+        resultado.set(
+            f"{c}°C -> {(c * 1.8) + 32:.2f}°F.\n"
+            f"{f}°F -> {(f - 32) / 1.8:.2f}°C.\n"
+            f"{c}°C -> {c + 273.15:.2f}°k.\n"
+            f"{k}°K -> {k - 273.15:.2f}°C."
+        )
+
+janela = tk.Tk()
+janela.title("Converter temperaturas")
+
+tk.Label(janela, text="Celsius").pack()
+entrada_c = tk.Entry(janela)
+entrada_c.pack()
+
+tk.Label(janela, text="Fahrenheit").pack()
+entrada_f = tk.Entry(janela)
+entrada_f.pack()
+
+tk.Label(janela, text="Kelvin").pack()
+entrada_k = tk.Entry(janela)
+entrada_k.pack() 
+
+tk.Button(janela, text= "Converter", command=converter).pack()
+
+resultado = tk.StringVar()
+tk.Label(janela, textvariable=resultado).pack()
+
+janela.mainloop()
---
+++ temperatura.py
@@ -0,0 +1,7 @@
+class Temperatura:
+
+    def __init__(self, c, f, k):
+        self.c = c
+        self.f = f
+        self.k = k
+
